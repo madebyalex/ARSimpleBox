9b875a155cd690763d0b4b35fcf3ff9a7a6b6f71
Alexander <hello@alexander.works>
2020-08-29 17:20:50 +0300

    A cube changes its color to a random one after tapping it.

---
+++ SimpleBox/Controllers/SimpleBoxWithTouchVC.swift
@@ -0,0 +1,99 @@
+//
+//  SimpleBoxWithTouchVC.swift
+//  SimpleBox
+//
+//  Created by Alexander on 29.08.2020.
+//  Copyright © 2020 Alexander Nuzhnyi. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class SimpleBoxWithTouchVC: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene()
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+        
+        // Adding a 3D object
+        addObject()
+        
+        // Recognizing a touch
+        recognizeTap()
+        
+        // Adding some default lighting
+        configureLighting()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    // Adding some objects
+    func addObject() {
+        let box = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0.01)
+        
+        let material = SCNMaterial()
+        material.name = "Color"
+        material.diffuse.contents = UIImage(named: "wood.jpg")
+        box.materials = [material]
+        
+        let boxNode = SCNNode(geometry: box)
+        boxNode.position = SCNVector3(0, 0, -0.75)
+        
+        sceneView.scene.rootNode.addChildNode(boxNode)
+    }
+    
+    // Recognizing a touch
+    func recognizeTap() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func tapped(recognizer: UIGestureRecognizer) {
+        let sceneView = recognizer.view as! SCNView
+        let touchLocation = recognizer.location(in: sceneView)
+        let hitResults = sceneView.hitTest(touchLocation, options: [:])
+        
+        if !hitResults.isEmpty {
+            let node = hitResults[0].node
+            let material = node.geometry?.material(named: "Color")
+            material?.diffuse.contents = UIColor.random()
+        }
+    }
+    
+    // Adding Basic Lighting
+    func configureLighting() {
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.automaticallyUpdatesLighting = true
+    }
+
+}
